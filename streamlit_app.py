faa8e9eae58446f1d193d45a362dc0860dc3c533
mtjossi <45386128+mtjossi@users.noreply.github.com>
2022-04-18 10:27:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ model_choice = st.radio("Select Models:", ['1', '2', '3', 'Suggested'])
 
 ac = np.arange(1,50001)
 
-model_dict = {"1":"No discounts, but buy in "}
+# model_dict = {"1":"No discounts, but buy in packs of200"}
 
 ###################################1
 ###################################1
@@ -22,6 +22,7 @@ if model_choice == '1':
     st.write("No discounts, and Actions are purchased in packs of 200")
 
     ppa = st.slider("Test Price per Action:", min_value=0.00, max_value=.1, value=0.030, step=0.005, key=1)
+    max_cpa = st.number_input("Max Cost per Action", min_value=0.001, max_value=0.1, value=0.0054)
     income_list = np.array([])
     income = ppa*200
     increment = ppa*200
@@ -35,18 +36,20 @@ if model_choice == '1':
 
     df_200_no = pd.DataFrame({'actions':ac,
                   'income':income_list})
-
-    df_200_no['action_cost'] = 0.0033 * df_200_no['actions']
-    df_200_no['wa_cost'] = 0.005 * df_200_no['actions']
+    cpa = np.linspace(0.0033, max_cpa, 50000)
+    df_200_no['action_cost'] = cpa * df_200_no['actions']
+    df_200_no['wa_cost'] = (0.005+0.00185) * df_200_no['actions']
     df_200_no['total_cost'] = df_200_no['action_cost'] + df_200_no['wa_cost']
     df_200_no['profit'] = ((df_200_no['income'] - df_200_no['total_cost'])/df_200_no['income'])*100
     df_200_no['profit2'] = ((df_200_no['income'] - df_200_no['action_cost'])/df_200_no['income'])*100
 
-    a1, a2, a3, a4 = st.columns(4)
-    a1.metric('Profit with WhatsApp', value=f"{np.round(df_200_no['profit'].tail(1).values[0], 2)}%")
-    a2.metric('Profit without WhatsApp', value=f"{np.round(df_200_no['profit2'].tail(1).values[0], 2)}%")
+    a1, a2, a3, a4, a5 = st.columns(5)
+    a1.metric('Profit with costs', value=f"{np.round(df_200_no['profit'].tail(1).values[0], 2)}%")
+    a2.metric('Profit without costs', value=f"{np.round(df_200_no['profit2'].tail(1).values[0], 2)}%")
     a3.metric('Price per Action', value=f"{ppa:.3f}")
-    a4.metric('Pack Price', value=f"${ppa*200:.2f}")
+    a4.metric('Max Cost per Action', value=f"{max_cpa:.4f}")
+    a5.metric('Pack Price', value=f"${ppa*200:.2f}")
+    st.write("Costs = Twilio WhatsApp ($0.005) + SendInBlue email ($0.00185)")
 
 
     fig = go.Figure()
@@ -73,18 +76,19 @@ if model_choice == '1':
 
     df_1000_no = pd.DataFrame({'actions':ac,
                   'income':income_list2})
-
-    df_1000_no['action_cost'] = 0.0033 * df_1000_no['actions']
-    df_1000_no['wa_cost'] = 0.005 * df_1000_no['actions']
+    df_1000_no['action_cost'] = cpa * df_1000_no['actions']
+    df_1000_no['wa_cost'] = (0.005+0.00185) * df_1000_no['actions']
     df_1000_no['total_cost'] = df_1000_no['action_cost'] + df_1000_no['wa_cost']
     df_1000_no['profit'] = ((df_1000_no['income'] - df_1000_no['total_cost'])/df_1000_no['income'])*100
     df_1000_no['profit2'] = ((df_1000_no['income'] - df_1000_no['action_cost'])/df_1000_no['income'])*100
 
-    a1, a2, a3, a4 = st.columns(4)
-    a1.metric('Profit with WhatsApp', value=f"{np.round(df_1000_no['profit'].tail(1).values[0], 2)}%")
-    a2.metric('Profit without WhatsApp', value=f"{np.round(df_1000_no['profit2'].tail(1).values[0], 2)}%")
+    a1, a2, a3, a4, a5 = st.columns(5)
+    a1.metric('Profit with costs', value=f"{np.round(df_1000_no['profit'].tail(1).values[0], 2)}%")
+    a2.metric('Profit without costs', value=f"{np.round(df_1000_no['profit2'].tail(1).values[0], 2)}%")
     a3.metric('Price per Action', value=f"{ppa2:.3f}")
-    a4.metric('Pack Price', value=f"${ppa2*1000:.2f}")
+    a4.metric('Max Cost per Action', value=f"{max_cpa:.4f}")
+    a5.metric('Pack Price', value=f"${ppa2*200:.2f}")
+    st.write("Costs = Twilio WhatsApp ($0.005) + SendInBlue email ($0.00185)")
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df_1000_no['actions'], y=df_1000_no['income'], name='income', mode='lines', line={'color':'blue'}))
@@ -101,6 +105,7 @@ if model_choice == '2':
     st.write("2) 1 x 1000 Pack with 1 free 200 Pack")
 
     ppa = st.slider("Test Price per Action:", min_value=0.0, max_value=.1, value=0.03, step=0.005, key=3)
+    max_cpa = st.number_input("Max Cost per Action", min_value=0.001, max_value=0.1, value=0.0054)
     income_list = np.array([])
     income = ppa*200
     increment = ppa*200
@@ -120,18 +125,20 @@ if model_choice == '2':
     st.subheader("1) 10 x 200 Pack with 1 free 200 Pack")
     df_200_disc = pd.DataFrame({'actions':ac,
                   'income':income_list})
-
-    df_200_disc['action_cost'] = 0.0033 * df_200_disc['actions']
-    df_200_disc['wa_cost'] = 0.005 * df_200_disc['actions']
+    cpa = np.linspace(0.0033, max_cpa, 50000)
+    df_200_disc['action_cost'] = cpa * df_200_disc['actions']
+    df_200_disc['wa_cost'] = (0.005+0.00185) * df_200_disc['actions']
     df_200_disc['total_cost'] = df_200_disc['action_cost'] + df_200_disc['wa_cost']
     df_200_disc['profit'] = ((df_200_disc['income'] - df_200_disc['total_cost'])/df_200_disc['income'])*100
     df_200_disc['profit2'] = ((df_200_disc['income'] - df_200_disc['action_cost'])/df_200_disc['income'])*100
 
-    a1, a2, a3, a4 = st.columns(4)
-    a1.metric('Profit with WhatsApp', value=f"{np.round(df_200_disc['profit'].tail(1).values[0], 2)}%")
-    a2.metric('Profit without WhatsApp', value=f"{np.round(df_200_disc['profit2'].tail(1).values[0], 2)}%")
+    a1, a2, a3, a4, a5 = st.columns(5)
+    a1.metric('Profit with costs', value=f"{np.round(df_200_disc['profit'].tail(1).values[0], 2)}%")
+    a2.metric('Profit without costs', value=f"{np.round(df_200_disc['profit2'].tail(1).values[0], 2)}%")
     a3.metric('Price per Action', value=f"{ppa:.3f}")
-    a4.metric('Pack Price', value=f"${ppa*200:.2f}")
+    a4.metric('Max Cost per Action', value=f"{max_cpa:.4f}")
+    a5.metric('Pack Price', value=f"${ppa*200:.2f}")
+    st.write("Costs = Twilio WhatsApp ($0.005) + SendInBlue email ($0.00185)")
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df_200_disc['actions'][:10000], y=df_200_disc['income'][:10000], name='income', mode='lines', line={'color':'blue'}))
@@ -163,17 +170,19 @@ if model_choice == '2':
     df_1000_disc = pd.DataFrame({'actions':ac,
                   'income':income_list2})
 
-    df_1000_disc['action_cost'] = 0.0033 * df_1000_disc['actions']
-    df_1000_disc['wa_cost'] = 0.005 * df_1000_disc['actions']
+    df_1000_disc['action_cost'] = cpa * df_1000_disc['actions']
+    df_1000_disc['wa_cost'] = (0.005+0.00185) * df_1000_disc['actions']
     df_1000_disc['total_cost'] = df_1000_disc['action_cost'] + df_1000_disc['wa_cost']
     df_1000_disc['profit'] = ((df_1000_disc['income'] - df_1000_disc['total_cost'])/df_1000_disc['income'])*100
     df_1000_disc['profit2'] = ((df_1000_disc['income'] - df_1000_disc['action_cost'])/df_1000_disc['income'])*100
 
-    a1, a2, a3, a4 = st.columns(4)
-    a1.metric('Profit with WhatsApp', value=f"{np.round(df_1000_disc['profit'].tail(1).values[0], 2)}%")
-    a2.metric('Profit without WhatsApp', value=f"{np.round(df_1000_disc['profit2'].tail(1).values[0], 2)}%")
+    a1, a2, a3, a4, a5 = st.columns(5)
+    a1.metric('Profit with costs', value=f"{np.round(df_1000_disc['profit'].tail(1).values[0], 2)}%")
+    a2.metric('Profit without costs', value=f"{np.round(df_1000_disc['profit2'].tail(1).values[0], 2)}%")
     a3.metric('Price per Action', value=f"{ppa2:.3f}")
-    a4.metric('Pack Price', value=f"${ppa2*1000:.2f}")
+    a4.metric('Max Cost per Action', value=f"{max_cpa:.4f}")
+    a5.metric('Pack Price', value=f"${ppa2*200:.2f}")
+    st.write("Costs = Twilio WhatsApp ($0.005) + SendInBlue email ($0.00185)")
 
     fig2 = go.Figure()
     fig2.add_trace(go.Scatter(x=df_1000_disc['actions'], y=df_1000_disc['income'], name='income', mode='lines', line={'color':'blue'}))
@@ -189,7 +198,9 @@ if model_choice == '3':
     st.write("1) 10 x 200 Pack with 1 free 100 Pack")
     st.write("2) 1 x 1000 Pack with 1 free 100 Pack")
 
+    
     ppa = st.slider("Test Price per Action:", min_value=0.0, max_value=.1, value=0.03, step=0.005, key=5)
+    max_cpa = st.number_input("Max Cost per Action", min_value=0.001, max_value=0.1, value=0.0054)
     income_list = np.array([])
     income = ppa*200
     increment = ppa*200
@@ -210,18 +221,20 @@ if model_choice == '3':
     st.subheader("1) 10 x 200 Pack with 1 free 100 Pack")
     df_200_100 = pd.DataFrame({'actions':ac[:47800],
                   'income':income_list})
-
-    df_200_100['action_cost'] = 0.0033 * df_200_100['actions']
-    df_200_100['wa_cost'] = 0.005 * df_200_100['actions']
+    cpa1 = np.linspace(0.0033, max_cpa, 50000)
+    df_200_100['action_cost'] = cpa1[:47800] * df_200_100['actions']
+    df_200_100['wa_cost'] = (0.005+0.00185) * df_200_100['actions']
     df_200_100['total_cost'] = df_200_100['action_cost'] + df_200_100['wa_cost']
     df_200_100['profit'] = ((df_200_100['income'] - df_200_100['total_cost'])/df_200_100['income'])*100
     df_200_100['profit2'] = ((df_200_100['income'] - df_200_100['action_cost'])/df_200_100['income'])*100
 
-    a1, a2, a3, a4 = st.columns(4)
-    a1.metric('Profit with WhatsApp', value=f"{np.round(df_200_100['profit'].tail(1).values[0], 2)}%")
-    a2.metric('Profit without WhatsApp', value=f"{np.round(df_200_100['profit2'].tail(1).values[0], 2)}%")
+    a1, a2, a3, a4, a5 = st.columns(5)
+    a1.metric('Profit with costs', value=f"{np.round(df_200_100['profit'].tail(1).values[0], 2)}%")
+    a2.metric('Profit without costs', value=f"{np.round(df_200_100['profit2'].tail(1).values[0], 2)}%")
     a3.metric('Price per Action', value=f"{ppa:.3f}")
-    a4.metric('Pack Price', value=f"${ppa*200:.2f}")
+    a4.metric('Max Cost per Action', value=f"{max_cpa:.4f}")
+    a5.metric('Pack Price', value=f"${ppa*200:.2f}")
+    st.write("Costs = Twilio WhatsApp ($0.005) + SendInBlue email ($0.00185)")
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df_200_100['actions'][:10000], y=df_200_100['income'][:10000], name='income', mode='lines', line={'color':'blue'}))
@@ -253,17 +266,20 @@ if model_choice == '3':
     df_1000_100 = pd.DataFrame({'actions':ac[:46500],
                   'income':income_list2})
 
-    df_1000_100['action_cost'] = 0.0033 * df_1000_100['actions']
-    df_1000_100['wa_cost'] = 0.005 * df_1000_100['actions']
+    cpa2 = np.linspace(0.0033, max_cpa, 50000)
+    df_1000_100['action_cost'] = cpa2[:46500] * df_1000_100['actions']
+    df_1000_100['wa_cost'] = (0.005+0.00185) * df_1000_100['actions']
     df_1000_100['total_cost'] = df_1000_100['action_cost'] + df_1000_100['wa_cost']
     df_1000_100['profit'] = ((df_1000_100['income'] - df_1000_100['total_cost'])/df_1000_100['income'])*100
     df_1000_100['profit2'] = ((df_1000_100['income'] - df_1000_100['action_cost'])/df_1000_100['income'])*100
 
-    a1, a2, a3, a4 = st.columns(4)
-    a1.metric('Profit with WhatsApp', value=f"{np.round(df_1000_100['profit'].tail(1).values[0], 2)}%")
-    a2.metric('Profit without WhatsApp', value=f"{np.round(df_1000_100['profit2'].tail(1).values[0], 2)}%")
+    a1, a2, a3, a4, a5 = st.columns(5)
+    a1.metric('Profit with costs', value=f"{np.round(df_1000_100['profit'].tail(1).values[0], 2)}%")
+    a2.metric('Profit without costs', value=f"{np.round(df_1000_100['profit2'].tail(1).values[0], 2)}%")
     a3.metric('Price per Action', value=f"{ppa2:.3f}")
-    a4.metric('Pack Price', value=f"${ppa2*1000:.2f}")
+    a4.metric('Max Cost per Action', value=f"{max_cpa:.4f}")
+    a5.metric('Pack Price', value=f"${ppa2*200:.2f}")
+    st.write("Costs = Twilio WhatsApp ($0.005) + SendInBlue email ($0.00185)")
 
     fig2 = go.Figure()
     fig2.add_trace(go.Scatter(x=df_1000_100['actions'], y=df_1000_100['income'], name='income', mode='lines', line={'color':'blue'}))
@@ -285,6 +301,7 @@ if model_choice == 'Suggested':
 
 
     ppa = st.slider("Test Price per Action:", min_value=0.0, max_value=.1, value=0.035, step=0.005, key=7)
+    max_cpa = st.number_input("Max Cost per Action", min_value=0.001, max_value=0.1, value=0.0054)
     income_list = np.array([])
     income = ppa*200
     increment = ppa*200
@@ -306,19 +323,22 @@ if model_choice == 'Suggested':
     df_200_100 = pd.DataFrame({'actions':ac[:47800],
                   'income':income_list})
 
-    df_200_100['action_cost'] = 0.0033 * df_200_100['actions']
-    df_200_100['wa_cost'] = 0.005 * df_200_100['actions']
+    cpa1 = np.linspace(0.0033, max_cpa, 50000)
+    df_200_100['action_cost'] = cpa1[:47800] * df_200_100['actions']
+    df_200_100['wa_cost'] = (0.005+0.00185) * df_200_100['actions']
     df_200_100['total_cost'] = df_200_100['action_cost'] + df_200_100['wa_cost']
     df_200_100['profit'] = ((df_200_100['income'] - df_200_100['total_cost'])/df_200_100['income'])*100
     df_200_100['profit2'] = ((df_200_100['income'] - df_200_100['action_cost'])/df_200_100['income'])*100
     profit_3 = df_200_100.loc[2099, 'income'] - df_200_100.loc[2099, 'total_cost']
     profit_4 = df_200_100.loc[2099, 'income'] - df_200_100.loc[2099, 'action_cost']
 
-    a1, a2, a3, a4 = st.columns(4)
-    a1.metric('Profit with WhatsApp', value=f"{np.round(df_200_100['profit'].tail(1).values[0], 2)}%")
-    a2.metric('Profit without WhatsApp', value=f"{np.round(df_200_100['profit2'].tail(1).values[0], 2)}%")
+    a1, a2, a3, a4, a5 = st.columns(5)
+    a1.metric('Profit with costs', value=f"{np.round(df_200_100['profit'].tail(1).values[0], 2)}%")
+    a2.metric('Profit without costs', value=f"{np.round(df_200_100['profit2'].tail(1).values[0], 2)}%")
     a3.metric('Price per Action', value=f"{ppa:.3f}")
-    a4.metric('Pack Price', value=f"${ppa*200:.2f}")
+    a4.metric('Max Cost per Action', value=f"{max_cpa:.4f}")
+    a5.metric('Pack Price', value=f"${ppa*200:.2f}")
+    st.write("Costs = Twilio WhatsApp ($0.005) + SendInBlue email ($0.00185)")
 
     min_prof = np.round(df_200_100['profit'].tail(1).values[0], 2)
     max_prof = np.round(df_200_100['profit2'].tail(1).values[0], 2)
@@ -355,20 +375,22 @@ if model_choice == 'Suggested':
 
     df_1000_100 = pd.DataFrame({'actions':ac[:46500],
                   'income':income_list2})
-
-    df_1000_100['action_cost'] = 0.0033 * df_1000_100['actions']
-    df_1000_100['wa_cost'] = 0.005 * df_1000_100['actions']
+    cpa2 = np.linspace(0.0033, max_cpa, 50000)
+    df_1000_100['action_cost'] = cpa2[:46500] * df_1000_100['actions']
+    df_1000_100['wa_cost'] = (0.005+0.00185) * df_1000_100['actions']
     df_1000_100['total_cost'] = df_1000_100['action_cost'] + df_1000_100['wa_cost']
     df_1000_100['profit'] = ((df_1000_100['income'] - df_1000_100['total_cost'])/df_1000_100['income'])*100
     df_1000_100['profit2'] = ((df_1000_100['income'] - df_1000_100['action_cost'])/df_1000_100['income'])*100
     profit_3_2 = df_1000_100.loc[1099, 'income'] - df_1000_100.loc[1099, 'total_cost']
     profit_4_2 = df_1000_100.loc[1099, 'income'] - df_1000_100.loc[1099, 'action_cost']
 
-    a1, a2, a3, a4 = st.columns(4)
-    a1.metric('Profit with WhatsApp', value=f"{np.round(df_1000_100['profit'].tail(1).values[0], 2)}%")
-    a2.metric('Profit without WhatsApp', value=f"{np.round(df_1000_100['profit2'].tail(1).values[0], 2)}%")
+    a1, a2, a3, a4, a5 = st.columns(5)
+    a1.metric('Profit with costs', value=f"{np.round(df_1000_100['profit'].tail(1).values[0], 2)}%")
+    a2.metric('Profit without costs', value=f"{np.round(df_1000_100['profit2'].tail(1).values[0], 2)}%")
     a3.metric('Price per Action', value=f"{ppa2:.3f}")
-    a4.metric('Pack Price', value=f"${ppa2*1000:.2f}")
+    a4.metric('Max Cost per Action', value=f"{max_cpa:.4f}")
+    a5.metric('Pack Price', value=f"${ppa2*200:.2f}")
+    st.write("Costs = Twilio WhatsApp ($0.005) + SendInBlue email ($0.00185)")
 
     min_prof2 = np.round(df_1000_100['profit'].tail(1).values[0], 2)
     max_prof2 = np.round(df_1000_100['profit2'].tail(1).values[0], 2)
